f986f38735426bea6b9882180163b4f94c10efeb
alitu <alym11731996@gmail.com>
2024-03-03 08:05:49 +0300

    Wait strategy

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -28,19 +30,16 @@ public class LoginTests {
         WebElement cookies= driver.findElement(By.xpath("//*[@id='onetrust-accept-btn-handler']"));
         cookies.click();
 
-        Thread.sleep(2000);
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5L)); //koşulların oluşması için bekleyeceğim süre
 
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("myAccount"))); //bahsettiğim element oluşmuş mu kontrolü
         driver.findElement(By.xpath("//*[@id='myAccount']")).click();
 
-        Thread.sleep(2000);
-
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@id='login']")));
         driver.findElement(By.xpath("//a[@id='login']")).click();
 
-        Thread.sleep(2000);
-
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='txtUserName']")));
         driver.findElement(By.xpath("//*[@id='txtUserName']")).sendKeys("aliturkmenn9@gmail.com", Keys.ENTER);
-
-        Thread.sleep(2000);
 
     }
 
