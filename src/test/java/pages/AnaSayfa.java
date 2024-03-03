59004548768bbb02ddc2d2667093a9ab7d55617d
alitu <alym11731996@gmail.com>
2024-03-03 14:31:48 +0300

    Search area test

--- src/test/java/pages/AnaSayfa.java
+++ src/test/java/pages/AnaSayfa.java
@@ -6,7 +6,7 @@ import pages.Sayfa;
 public class AnaSayfa extends Sayfa {
     public final String hesabim="myAccount"; //final ile verdiğim string in farklı sınıflarda değiştirilmesi engellenir!
     public final String girisYap="login";
-    public final String cookies="onetrust-accept-btn-handler";
+    public final String urunArama="[placeholder='Ürün, kategori veya marka ara']";
 
     public AnaSayfa(WebDriver driver) {
         super(driver);
