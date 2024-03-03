ea9bc1064b82fda08af08449514e799006218319
alitu <alym11731996@gmail.com>
2024-03-03 14:32:02 +0300

    Search area test

--- src/test/java/pages/GirisYapSayfasi.java
+++ src/test/java/pages/GirisYapSayfasi.java
@@ -5,7 +5,7 @@ import pages.Sayfa;
 
 public class GirisYapSayfasi extends Sayfa {
     public final String emailAdresi="txtUserName";
-    public final String girisYolu="btnLogin";
+    public final String girisYapTusu="btnLogin";
 
     public GirisYapSayfasi(WebDriver driver) {
         super(driver);
